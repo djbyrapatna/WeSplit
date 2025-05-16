c42b3f6b264ff46998dde19c627fbe2a6a8b0fcd
Dhruva Byrapatna <dhruvajb@gmail.com>
2025-05-16 13:24:38 -0400

    Initial commit

---
+++ WeSplitTests/WeSplitTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitTests.swift
+//  WeSplitTests
+//
+//  Created by Dhruva Jothik Byrapatna on 5/16/25.
+//
+
+import Testing
+@testable import WeSplit
+
+struct WeSplitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
